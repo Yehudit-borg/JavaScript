5b2aa8a9cd4dfc46fd306bc24b769474f314c1a2
Yehudit-borg <mariettejudy@yahoo.com>
2021-05-21 16:45:56 +0100

    Commit

--- main.js
+++ main.js
@@ -24,3 +24,44 @@ function convertFahrToCelsius(F){
   console.log(convertFahrToCelsius(false))
   console.log(convertFahrToCelsius({temp: 0}))
   console.log(convertFahrToCelsius([1,2,3]));
+
+
+
+
+  function checkYuGiOh(n){
+    var l_arr = [] ; 
+    var arr = 1, arr2 = 1, arr3 = 0;
+  
+  
+  if(!isNaN(n)){
+    for (arr; arr <=n; arr++){
+    arr2 = arr; 
+    arr3 = 0;
+    if (arr % 2 == 0) {
+      arr2 = "yu"; arr3++;
+    } 
+    if (arr % 3 == 0) {
+      arr2 = "gi"; arr3++;
+    } 
+    if (arr % 5 == 0) {
+      arr2 = "oh"; arr3++;
+    } 
+    
+    if (arr3 == 2) {
+      arr2 = "yu-oh"; 
+    } 
+    if (arr3 == 3) {
+      arr2 = "yu-gi-oh"; 
+    } 
+    l_arr.push(arr2)
+    }
+    console.log(l_arr);
+  }
+  else{console.log("Invalid parameter " + JSON.stringify(n))}
+  }
+  
+  checkYuGiOh(10)
+  checkYuGiOh(100)
+  checkYuGiOh("fizzbuzz is meh")
+  checkYuGiOh("12")
+  checkYuGiOh(true)
